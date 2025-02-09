24db2c205a3f17c3e78fddac81f0eed5c9448715
Mirno Hoogendonk <mirno_hoogendonk@hotmail.com>
2025-02-09 15:58:33 +0100

    fix: gosec for echo server + webroot demo prep
    fix gosec
    prepare webroot folder using embed

--- cmd/simple-echo-server/main.go
+++ cmd/simple-echo-server/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+//go:embed webroot/*
+var embedWebRoot embed.FS
+
+const (
+	timeoutDefault = 10
+	port           = "8085"
+	acmeWebroot    = "./webroot" // Directory to store ACME challenge files
+
+)
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Echo!")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/.well-known/acme-challenge/", handler)
-	fmt.Println("Starting server on :8085")
-	err := http.ListenAndServe(":8085", nil)
+
+	mux := http.NewServeMux()
+	// muecho handler
+	mux.HandleFunc("/", echoHandler)
+
+	// Serve ACME challenges from a directory
+	acmeHandler := http.StripPrefix("/.well-known/acme-challenge/", http.FileServer(http.FS(embedWebRoot))) // http.Dir(acmeWebroot)
+	mux.Handle("/.well-known/acme-challenge/", acmeHandler)
+
+	server := &http.Server{
+		Addr:         "0.0.0.0:" + port,
+		Handler:      mux,
+		ReadTimeout:  timeoutDefault * time.Second, // Maximum duration for reading the request
+		WriteTimeout: timeoutDefault * time.Second, // Maximum duration for writing the response
+		IdleTimeout:  timeoutDefault * time.Second, // Maximum duration for idle connections
+	}
+
+	fmt.Println("Starting server on :" + port)
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		fmt.Println("shutting down server...")
